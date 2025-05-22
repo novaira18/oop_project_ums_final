c6c1d6589208962ccd7dc4edd63f068fc446d1e4
novaira18 <novairawaseemrizvi@gmail.com>
2025-05-22 14:25:58 +0500

    Update FacultyMemberManager.h

--- FacultyMemberManager.h
+++ FacultyMemberManager.h
@@ -1,5 +1,7 @@
 #include <vector>
 #include <string>
+#include "FilePaths.h"
+
 using namespace std;
 
 struct FacultyMember {
@@ -8,12 +10,21 @@ struct FacultyMember {
     string deptID;
     string position;
 };
+
 class FacultyMemberManager {
 private:
     vector<FacultyMember> facultyMembers;
+    string facultyID;
+    string membersFileName;
+
+    void loadFromFile();
+    void saveToFile();
+
 public:
+    FacultyMemberManager(const string& facultyID);
     void addFacultyMember();
     void viewFacultyMembers();
     void editFacultyMember(const string& memberID);
     void deleteFacultyMember(const string& memberID);
+    FacultyMember* findFacultyMember(const string& memberID);
 };
