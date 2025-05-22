1f7e67fe848364677e3cc43bb62d0d5b621dd867
novaira18 <novairawaseemrizvi@gmail.com>
2025-05-22 14:09:26 +0500

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -2,13 +2,19 @@
 
 class Admin : public User {
 public:
+    // Constructor
+    Admin(string name, string email, string cnic, string registrationId,
+          string username, string password, string userID,
+          string role, string contactNumber);
+
     void showPanel() override;
+
     void manageFaculties();
     void manageDepartments();
     void manageStudents();
     void manageFacultyMembers();
     void manageCourses();
     void manageCredentials();
-    void manageUGF();
+    void manageUG();
     void manageSocieties();
 };
