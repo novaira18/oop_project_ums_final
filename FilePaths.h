430a231f6dc3f847eedc8af17b48c4f2a43ee4f2
novaira18 <novairawaseemrizvi@gmail.com>
2025-05-22 12:20:38 +0500

    Create FilePaths.h

---
+++ FilePaths.h
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+const string STUDENT_DATA_FILE = "data/students.txt";
+const string FACULTY_DATA_FILE = "data/faculty.txt";
+const string DEPARTMENT_DATA_FILE = "data/departments.txt";
+const string COURSE_DATA_FILE = "data/courses.txt";
+const string CREDENTIALS_FILE = "data/credentials.txt";
+const string UG_MEMBERS_FILE = "data/ug_members.txt";
+const string SOCIETIES_FILE = "data/societies.txt";
