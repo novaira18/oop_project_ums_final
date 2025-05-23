29556b30a735a899e9476fa3afdf0b7e7f64c461
novaira18 <novairawaseemrizvi@gmail.com>
2025-05-24 01:24:04 +0500

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,55 +1,43 @@
-#include "Admin.h"
 #include <iostream>
+#include "admin.h"
+#include "FacultyManager.h"
+#include "DepartmentManager.h"
+#include "StudentManager.h"
+#include "FacultyMemberManager.h"
+#include "CourseManager.h"
+#include "CredentialManager.h"
+#include "UGManager.h"
+#include "SocietyManager.h"
+
 using namespace std;
 
-// Constructor definition
-Admin::Admin(string name, string email, string cnic, string registrationId,
-             string username, string password, string userID,
-             string role, string contactNumber)
-    : User(name, email, cnic, registrationId, username, password, userID, role, contactNumber) { }
+void Admin::showMenu() {
+    int choice;
+    do {
+        cout << "\n👑 ADMIN PANEL\n";
+        cout << "[1] Manage Faculties\n";
+        cout << "[2] Manage Departments\n";
+        cout << "[3] Manage Students\n";
+        cout << "[4] Manage Faculty Members\n";
+        cout << "[5] Courses Management\n";
+        cout << "[6] Manage User Credentials\n";
+        cout << "[7] University General Management\n";
+        cout << "[8] Societies Management\n";
+        cout << "[9] Logout\n";
+        cout << "Enter choice: ";
+        cin >> choice;
 
-// Show Panel
-void Admin::showPanel() {
-    cout << "Admin Panel\n";
-    cout << "1. Manage Faculties\n";
-    cout << "2. Manage Departments\n";
-    cout << "3. Manage Students\n";
-    cout << "4. Manage Faculty Members\n";
-    cout << "5. Manage Courses\n";
-    cout << "6. Manage Credentials\n";
-    cout << "7. Manage University General\n";
-    cout << "8. Manage Societies\n";
-}
-
-// Stub implementations
-void Admin::manageFaculties() {
-    cout << "Managing faculties...\n";
-}
-
-void Admin::manageDepartments() {
-    cout << "Managing departments...\n";
-}
-
-void Admin::manageStudents() {
-    cout << "Managing students...\n";
-}
-
-void Admin::manageFacultyMembers() {
-    cout << "Managing faculty members...\n";
-}
-
-void Admin::manageCourses() {
-    cout << "Managing courses...\n";
-}
-
-void Admin::manageCredentials() {
-    cout << "Managing credentials...\n";
-}
-
-void Admin::manageUG() {
-    cout << "Managing UGF...\n";
-}
-
-void Admin::manageSocieties() {
-    cout << "Managing societies...\n";
+        switch (choice) {
+            case 1: FacultyManager().menu(); break;
+            case 2: DepartmentManager().menu(); break;
+            case 3: StudentManager().menu(); break;
+            case 4: FacultyMemberManager().menu(); break;
+            case 5: CourseManager().menu(); break;
+            case 6: CredentialManager().menu(); break;
+            case 7: UGManager().menu(); break;
+            case 8: SocietyManager().menu(); break;
+            case 9: cout << "Logging out...\n"; break;
+            default: cout << "Invalid option. Try again.\n";
+        }
+    } while (choice != 9);
 }
