7813014bcb0d07682886c8f1329cc8536e866f4e
novaira18 <novairawaseemrizvi@gmail.com>
2025-05-27 13:05:41 +0500

    Create Faculty.h

---
+++ Faculty.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+// Abstract Base Class
+class Faculty {
+public:
+    virtual void viewPersonalInfo() = 0;
+    virtual void viewAssignedCourses() = 0;
+    virtual void viewEnrolledStudents() = 0;
+    virtual void markAttendance() = 0;
+    virtual void assignGrades() = 0;
+
+    virtual ~Faculty() {}
+};
+
+// Subclass for Viewing Faculty Information
+class FacultyInfoViewer : public Faculty {
+public:
+    void viewPersonalInfo() override;
+    void viewAssignedCourses() override;
+    void viewEnrolledStudents() override;
+};
+
+// Subclass for Faculty Actions (Attendance and Grades)
+class FacultyActionManager : public Faculty {
+public:
+    void markAttendance() override;
+    void assignGrades() override;
+};
+
