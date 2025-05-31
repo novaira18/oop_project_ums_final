1e9c231d1703528f8902b25e165dbc814a906af8
novaira18 <novairawaseemrizvi@gmail.com>
2025-05-31 16:48:29 +0500

    Update UGManager.h

--- UGManager.h
+++ UGManager.h
@@ -1,75 +1,22 @@
-#ifndef UGM_H
-#define UGM_H
-
 #include <string>
+#include <vector>
 using namespace std;
 
-// Abstract Base Class
-class UGM {
-public:
-    // Events
-    virtual void Add() = 0;
-    virtual void View() = 0;
-    virtual void Edit() = 0;
-    virtual void Delete() = 0;
-
-    // Notice Board
-    virtual void Post(const string& filename) = 0;
-    virtual void ViewNotices() = 0;
-
-    // Library
-    virtual void AddBook() = 0;
-    virtual void ViewBooks() = 0;
-    virtual void IssueBook() = 0;
-    virtual void ReturnBook() = 0;
-
-    // Sports Complex
-    virtual void BookSlot() = 0;
-    virtual void ViewEquipment() = 0;
-
-    
-
-    // Hostel Info
-    virtual void AllotRoom() = 0;
-    virtual void ViewRoomData() = 0;
-
-    virtual ~UGM() {}
+struct UGMember {
+    string ugID;
+    string name;
+    string department;
+    string designation;
 };
-
-// ---------- Derived Sub-Classes ----------
-
-// Notice Board
-class NoticeBoard : public UGM {
+class UGManager {
+private:
+    vector<UGMember> ugMembers;
+    int findUGMemberIndex(const string& ugID);
+    void addUGMember();
+    void viewUGMembers();
+    void editUGMember();
+    void deleteUGMember();
 public:
-    void Post(const string& filename) override;
-    void ViewNotices() override;
+    void menu();  // Add this declaration
 };
-
-// Library
-class Library : public UGM {
-public:
-    void AddBook() override;
-    void ViewBooks() override;
-    void IssueBook() override;
-    void ReturnBook() override;
-};
-
-// Sports Complex
-class SportsComplex : public UGM {
-public:
-    void BookSlot() override;
-    void ViewEquipment() override;
-};
-
-// Hostel Info
-class HostelInfo : public UGM {
-public:
-    void AllotRoom() override;
-    void ViewRoomData() override;
-};
-
-
-
-#endif // UGM_H
-
-
+a
